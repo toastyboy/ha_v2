cfb36f5bec60206470c01aa96099b9f35853d5f1
toastyboy <dan.m.mcgrath@gmail.com>
2025-11-03 10:07:09 +0000

    files from home testpi

---
+++ pi/keepalive_niceproc.bash
@@ -0,0 +1,167 @@
+#!/bin/bash
+
+host=`hostname`
+url="http://varhost/varshare/"
+
+a=`ps -ef | grep keepalive | wc -l`
+if [ $a -gt 4 ]
+then echo "exit" ; exit 
+fi
+
+###Run these scripts each time this script runs
+
+./pingreboot.bash
+
+
+### scripts to monitor for
+
+#PINK
+if  [ `hostname` == "testpi" ] ;               
+then array=( "elec_meter_solar.py" "update_meter_solar.bash" "vitals.bash" "victron_read.py" "victron_read2.py" ) 
+fi
+
+#BLACK
+if  [ `hostname` == "black" ] ;
+then array=( "galaxy.pl" "temps.pl" "vitals.bash" )
+fi
+
+#CLEAR
+if  [ `hostname` == "clear" ] ;
+then array=( "process.py" "elec_meter.py" "update_meter.bash" \
+             "temps.pl" "vitals.bash" )
+fi
+
+#BLUE
+if  [ `hostname` == "blue" ] ;
+then array=( "pokeys_out.pl" "process.py" "vitals.bash" )
+fi
+
+#BUTLER
+if  [ `hostname` == "butler" ] ; 
+then array=( "doorbell.pl" "process.py" "pokeys_in.pl" "speedtest.bash" \
+             "vitals.bash" )
+fi
+
+#BOILER
+if  [ `hostname` == "boiler" ] ;
+then array=( "temps.pl" "vitals.bash" )
+fi
+
+#NAS
+if  [ `hostname` == "nas" ] ;
+then array=( "process.pl" "logic.pl" "vitals.bash" "heatmon.pl")
+fi
+
+#SHED
+if  [ `hostname` == "shed" ] ;
+then array=( "process.pl" "temps.pl" "vitals.bash" )
+fi
+
+#PI ZERO
+if  [ `hostname` == "pizero" ] ;
+then array=( "process.pl" "vitals.bash" "temps.pl" )
+fi
+
+# SOLAR
+if  [ `hostname` == "solar" ] ;
+then array=( "vitals.bash" "temps.pl" "elec_meter.py" "update_meter.bash" )
+fi
+
+#KEYPAD
+if  [ `hostname` == "keypad" ] ;
+then array=( "vitals.bash" "process.pl" "python_keypad.py" )
+fi
+
+#GARAGEPINK
+if  [ `hostname` == "garagepink" ] ;
+then array=( "process.pl" "vitals.bash" "arduino_www.bash" )
+fi
+
+#SMSHOST - - - GARAGEPI
+if  [ `hostname` == "smshost" ] ;
+then array=( "process_sms.pl" )
+fi
+
+
+#DNShost?
+
+#NAGIOS and other clusterpis
+
+
+
+
+
+#Actual work done here
+vars=`curl -s $url/varshow.php`
+varlines=`echo $vars | tr -d " \t\n\r" | sed s/"<br>"/"\n"/g` 
+
+for i in $varlines
+do
+  #echo $i
+  varname=`echo $i | awk -F"\|\|" '{ print $1 }'`
+  varval=`echo $i | awk -F'\|\|' '{ print $2 }' | awk -F"\|" '{ print $1 }'`
+
+  #echo "name "$varname ; echo "Val "$varval
+  declare $varname=$varval  # creates a variable with the name of $varname
+done
+
+#echo $vars
+
+## End up with 
+# dmx10||0||||1585588429
+# dmx11||1||||1585588430
+# dmx12||1||||1585588430
+#
+# $dmx10="0" ; $dmx11="1" etc.....
+
+
+for scriptname in "${array[@]}" 
+do
+
+  scriptnamesnip=`echo $scriptname | awk -F"." '{ print $1 }'`
+  #echo $scriptnamesnip
+  runvar=$scriptnamesnip"_"$host
+  #echo $runvar
+  #echo ${!runvar} # varable called scriptname (minus .suffix) _ hostname
+                   # contains RUN|STOP|RESTART 
+  
+  if [ -z "${!runvar}" ] # if no remote variable exists, create one
+  then
+    # echo "no remote var $runvar"
+    curl -sd "varname=$runvar&value=RUN&action=Send" $url/varstore.php    
+  fi
+
+
+  if [ "${!runvar}" == "RUN" ]
+  then 
+    #echo "$scriptname is RUN"
+    ps -ef | grep $scriptname | grep -v grep >> /dev/null
+    if [ $? == 1 ]
+    then  
+      nohup nice ./$scriptname &
+    fi
+
+  elif [ "${!runvar}" == "RESTART" ]
+  then
+    #echo "$scriptname is RESTART"
+    ps -ef | grep $scriptname | grep -v grep >> /dev/null
+    if [ $? == 0 ]
+    then
+      ps aux | grep -ie $scriptname | grep -v grep | \
+               awk '{ print $2 }' | xargs kill -9
+      sleep 1 
+    fi
+    nohup ./$scriptname &
+    curl -sd "varname=$runvar&value=RUN&action=Send" $url/varstore.php 
+
+  elif [ "${!runvar}" == "STOP" ]
+  then
+    #echo "$scriptname is STOP"
+    ps -ef | grep $scriptname | grep -v grep >> /dev/null
+    if [ $? == 0 ]
+    then 
+      ps aux | grep -ie $scriptname | grep -v grep | \
+               awk '{ print $2 }' | xargs kill -9 
+    fi
+  fi
+done 
---
+++ pi/victron_read2.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+
+import serial
+import re
+import requests
+import time
+
+
+#set up serial port
+ser = serial.Serial(
+    port='/dev/ttyUSB1',\
+    baudrate=19200,\
+    parity=serial.PARITY_NONE,\
+    stopbits=serial.STOPBITS_ONE,\
+    bytesize=serial.EIGHTBITS,\
+        timeout=0)
+
+print("connected to: " + ser.portstr)
+
+url='http://varhost/varshare/varstore.php'
+m=0
+n=0
+p=0
+q=0
+max=10
+
+#watchdog timers 
+time1 = time.time()
+time2 = time1
+time3 = time1
+time4 = time1
+time5 = time1
+
+
+while True:
+     cc=str(ser.readline())
+     print(cc)
+     print("--")
+     if cc.startswith("b'VPV\\t"):
+       try:
+         time2 = time.time() 
+         print(time2)  
+         info, value = cc.split("\\t", maxsplit=2)
+         solarvolts = re.sub(r'[^0-9]', '', value)
+         solarvolts = int(solarvolts)
+         solarvolts = solarvolts / 1000       
+         print ("solar voltage" , solarvolts)
+
+         if ( m > max ):
+           myobj = {'varname': 'mppt2_solarvolts' ,
+                    'value': solarvolts,
+                    'action' : 'Send' }
+           x = requests.post(url, data = myobj)
+           m=0
+       except:
+         print ("Out of bound VPV!")
+
+
+     elif cc.startswith("b'PPV\\t"):
+       try:
+         time3 = time.time()
+         info, value = cc.split("\\t", maxsplit=2)
+         solarpower = re.sub(r'[^0-9]', '', value)
+         solarpower = int(solarpower)
+         #solarpower = solarpower / 1000
+         print ("solar power" , solarpower)
+
+         if ( n > max):
+           myobj = {'varname': 'mppt2_solarpower' ,
+                    'value': solarpower,
+                    'action' : 'Send' }
+           x = requests.post(url, data = myobj)
+           n=0
+       except:
+         print ("Out of bound PPV!") 
+
+     elif cc.startswith("b'V\\t"):
+       try:
+         time4 = time.time()
+         info, value = cc.split("\\t", maxsplit=2)
+         chargevolts = re.sub(r'[^0-9]', '', value)
+         chargevolts = int(chargevolts)
+         chargevolts = chargevolts / 1000
+         print ("MPPT charge voltage VPV" , chargevolts)
+
+         if ( p > max):
+           myobj = {'varname': 'mppt2_chargevolts' ,
+                    'value': chargevolts,
+                    'action' : 'Send' }
+           x = requests.post(url, data = myobj)
+           p=0
+       except:
+         print ("Out of bound V!") 
+
+     elif cc.startswith("b'I\\t"):
+       try:
+         time5 = time.time()
+         info, value = cc.split("\\t", maxsplit=2)
+         chargecurrent = re.sub(r'[^0-9]', '', value)
+         chargecurrent = int(chargecurrent)
+         chargecurrent = chargecurrent / 1000
+         print ("charge current" , chargecurrent)
+
+         if ( q > max):
+           myobj = {'varname': 'mppt2_chargecurrent' ,
+                    'value': chargecurrent,
+                    'action' : 'Send' }
+           x = requests.post(url, data = myobj)
+           q=0
+       except:
+         print ("Out of bound I!") 
+     m=m+1
+     n=n+1
+     p=p+1
+     q=q+1
+     if ( time.time() > (time2 + 60) ):
+       print("No action, quitting")
+       quit()
+     if ( time.time() > (time3 + 60) ):
+       print("No action, quitting")
+       quit()
+     if ( time.time() > (time4 + 60) ):
+       print("No action, quitting")
+       quit()
+     if ( time.time() > (time5 + 60) ):
+       print("No action, quitting")
+       quit()
+
+     time.sleep(0.1)
